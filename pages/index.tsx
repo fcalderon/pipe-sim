c6284f762ca3a83dd6e684d23579e4a0614ad270
Francisco Calderon <fjavier5152@gmail.com>
2022-06-12 15:08:22 -0400

    feat: start adding block state

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,67 +2,212 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import { bool } from 'prop-types';
+
+export class Location {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class BlockNeighbors {
+  top?: Block;
+  bottom?: Block;
+  left?: Block;
+  right?: Block;
+
+  constructor(top?: Block, bottom?: Block, left?: Block, right?: Block) {
+    this.top = top;
+    this.bottom = bottom;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+
+export class Block {
+  height = 200;
+  width = 200;
+  ticked = false;
+  location: Location;
+  content: string;
+  grid?: Grid;
+
+  constructor(location: Location, ticked: boolean = false,
+              content: any, grid?: Grid) {
+    this.location = location;
+    this.ticked = ticked;
+    this.content = content || 'blue';
+    this.grid = grid;
+  }
+
+  getTopNeighbor(): Block | null | undefined {
+    return this.grid?.getBlockIfExists(this.location.x  - 1, this.location.y);
+  }
+
+  getBottomNeighbor(): Block | null | undefined {
+    return this.grid?.getBlockIfExists(this.location.x + 1, this.location.y);
+
+  }
+  getLeftNeighbor(): Block | null | undefined {
+    return this.grid?.getBlockIfExists(this.location.x, this.location.y  - 1);
+  }
+  getRightNeighbor(): Block | null | undefined {
+    return this.grid?.getBlockIfExists(this.location.x, this.location.y  + 1);
+  }
+}
+
+export class Grid {
+  width = 10;
+  height = 10;
+  blocks: Array<Array<Block>> = [];
+
+
+  constructor(width: number = 10, height: number = 10, blocks?: Array<Array<Block>>) {
+    this.width = width;
+    this.height = height;
+
+    if (blocks) {
+      console.log('Constructing grid with blocks', blocks);
+      this.blocks = blocks;
+    } else {
+      this.blocks = new Array<Array<Block>>();
+
+      for (let x = 0; x < this.width; x++) {
+        const rowItems = new Array<Block>();
+        this.blocks.push(rowItems);
+        for (let y = 0; y < this.height; y++) {
+          rowItems.push(new Block(new Location(x, y), false, '', this))
+        }
+      }
+    }
+  }
+
+  getBlockIfExists(x: number, y: number): Block | null {
+    try {
+      return this.blocks[x][y]
+    } catch (e) {
+      return null;
+    }
+  }
+}
+
 
 const Home: NextPage = () => {
+  const [currentGrid, setCurrentGrid] = useState(new Grid());
+  const [count, setCount] = useState(0);
+
+  console.log('Current grid', {
+    grid: currentGrid,
+    block: currentGrid.blocks[0][0]
+  })
+
+  const updateBlock = (block: Block, event?: any) => {
+    if (event && event.buttons !== 1) {
+      return;
+    }
+
+    if (block.location) {
+
+      console.log('Updating grid block', {
+        block,
+        gridBlock: currentGrid.blocks,
+        grid: currentGrid,
+        count
+      });
+      currentGrid.blocks[block.location.x][block.location.y] = new Block(block.location,
+        !block.ticked, block.content === 'blue' ? 'red' : 'blue', currentGrid);
+
+      console.log('Updated block', currentGrid.blocks[block.location.x][block.location.y]);
+
+      setCurrentGrid(currentGrid);
+      setCount(count+1);
+    }
+
+  }
+  const renderBlock = (block: Block, index: number) => {
+    let state = block.ticked ? '*' : '';
+    let top = block.getTopNeighbor();
+    let bottom = block.getBottomNeighbor();
+    let left = block.getLeftNeighbor();
+    let right = block.getRightNeighbor();
+
+    if (block.ticked) {
+      if (block.getTopNeighbor()?.ticked) {
+        state += '-T'
+      }
+
+      if (block.getRightNeighbor()?.ticked) {
+        state += '-R'
+      }
+
+      if (block.getBottomNeighbor()?.ticked) {
+        state += '-B'
+      }
+
+      if (block.getLeftNeighbor()?.ticked) {
+        state += '-L'
+      }
+    }
+
+    return (<div className={styles.pipeColumn} onMouseDown={() => updateBlock(block)}
+                 onMouseEnter={e => updateBlock(block, e)}
+                 datacontent={block.content}
+                 dataticked={`ticked=${block.ticked}`}
+                 datatickedcolor={`ticked-color=${block.ticked ? 'red' : 'blue'}`}
+                 style={{'backgroundColor': block.ticked ? 'red' : 'blue'}}>
+      <div><p>{block.location.x}, {block.location.y}</p></div>
+      <p>{state}</p>
+
+      {/*<div>*/}
+      {/*  {block?.neighbors?.top && block?.neighbors?.top.ticked && (<p>*</p>)}*/}
+      {/*  <p>*/}
+      {/*    {block?.neighbors?.left && block?.neighbors?.left.ticked && (<span>L</span>)} {block.location.x}, {block.location.y} - {block.ticked} - {block.content} {block?.neighbors?.right && block?.neighbors?.right.ticked && (<span>R</span>)}*/}
+      {/*  </p>*/}
+      {/*  {block?.neighbors?.bottom && block?.neighbors?.bottom.ticked && (<p>B</p>)}*/}
+      {/*</div>*/}
+    </div>)
+  };
+
+  const renderGrid = (grid: Grid) => {
+    console.log('Rendering grid', grid);
+    return grid.blocks
+      .map((row, index) => (<div className={styles.pipeRow} key={index}>
+        <div className={styles.pipeRow}>
+          {row.map((col, index) => (<div key={index}>{renderBlock(col, index)}</div>))}
+        </div>
+      </div>))
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Pipe Sim</title>
+        <meta name="description" content="Generated by create next app"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Pipe Simulator
         </h1>
         <h1>
           Shapes
         </h1>
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>Circle</h2>
-            <style>
-
-            </style>
-            <div className="circle"></div>
-
+        <div>
+          <div>
+            <h2>Square</h2>
+            {renderGrid(currentGrid)}
           </div>
-        </div>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <h2>Pipe</h2>
+            <div className={styles.pipe}></div>
+          </div>
         </div>
       </main>
 
@@ -74,7 +219,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
           </span>
         </a>
       </footer>
